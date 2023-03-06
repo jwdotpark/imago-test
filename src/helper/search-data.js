84e928cd1c7ec5f5d62af3a9c6adf93015a8b0cd
Jongwoo Park <jongwoo.park@code.berlin>
2023-03-06 12:28:45 +0100

    searchById for image page

--- src/helper/search-data.js
+++ src/helper/search-data.js
@@ -2,6 +2,7 @@ import mockData from "../../public/data/mock-data.json"
 
 export const searchData = (keyWord) => {
   let result = []
+
   mockData.item[1].response.map((item) => {
     const parsedBody = JSON.parse(item.body)
     const searchObject = parsedBody.caption
@@ -11,5 +12,21 @@ export const searchData = (keyWord) => {
       result.push(item)
     }
   })
+
+  return result
+}
+
+export const searchById = (id) => {
+  let result
+
+  mockData.item[1].response.map((item) => {
+    const parsedBody = JSON.parse(item.body)
+    const searchTarget = parsedBody.mediaid
+
+    if (searchTarget == id) {
+      result = parsedBody
+    }
+  })
+
   return result
 }
