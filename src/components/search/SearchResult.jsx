6a8d8adfaeb3dbcd5480d9f205c9ecfb254e8538
Jongwoo Park <jongwoo.park@code.berlin>
2023-03-06 11:56:55 +0100

    search result image grid added

---
+++ src/components/search/SearchResult.jsx
@@ -0,0 +1,33 @@
+import { Box, Image } from "@chakra-ui/react"
+
+const SearchResult = ({ searchValue }) => {
+  const parseBody = (body) => {
+    return JSON.parse(body)
+  }
+
+  return (
+    <Box>
+      <Box
+        padding={4}
+        mx="auto"
+        sx={{ columnCount: [1, 2, 3], columnGap: "8px" }}
+      >
+        {searchValue.map((item) => {
+          return (
+            <Image
+              key={item.name}
+              w="100%"
+              h="100%"
+              src={parseBody(item.body).mediasrc}
+              alt={item.name}
+              mb={2}
+              display="inline-block"
+            />
+          )
+        })}
+      </Box>
+    </Box>
+  )
+}
+
+export default SearchResult
